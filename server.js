ba85165fc67befb57a55eda68678ac84cd8589e4
Peter Suh <petersuh@Peters-MacBook-Pro.local>
2022-03-19 08:29:24 -0300

    adding api

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 //server file that works
-
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+connectDB();
+
 app.get('/', (req, res) => res.send('API Running'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => console.log(`Sever started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-//testing
