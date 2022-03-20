5ff5f69fac57a7fe6dec4fd9adf3b3e7034db133
Peter Suh <petersuh@Peters-MacBook-Pro.local>
2022-03-20 14:23:14 -0300

    api post working-users

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 
 connectDB();
 
+//init middleware
+
+app.use(express.json({ extended: false }));
+
+
 app.get('/', (req, res) => res.send('API Running'));
 
 app.use('/api/users', require('./routes/api/users'));
