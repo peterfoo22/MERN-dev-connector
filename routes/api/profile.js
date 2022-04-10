7d24c8024bdfcbfd8d67ed14341559b3bc8e48e8
Peter Suh <petersuh@Peters-MacBook-Pro-2.local>
2022-04-10 16:25:11 -0300

    update to profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,13 +1,27 @@
 const express = require('express');
-
+const auth = require('../../middleware/auth');
 const router = express.Router();
+const User = require('../../models/User')
 
+const Profile = require('../../models/Profile');
 
-// @route  GET api/profile
+// @route  GET api/profile/me
 // @desc   Test route
 // @access  Public
 
 
-router.get('/', (req, res) => res.send('Profile Route'));
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name','avatar']);
+
+    if(!profile) {
+      return res.status(400).json({msg:'There is no profile for the user'});    
+    }
+
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send    
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 //server file that works
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
+// Connect Database
 connectDB();
 
-//init middleware
+// Init Middleware
+app.use(express.json());
 
-app.use(express.json({ extended: false }));
-
-
-app.get('/', (req, res) => res.send('API Running'));
-
+// Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
@@ -22,4 +21,3 @@ app.use('/api/posts', require('./routes/api/posts'));
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
