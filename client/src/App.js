37db4105e15c72cb0546d42454e3d05ce0c5cd29
Peter Suh <petersuh@Peters-MacBook-Pro-2.local>
2022-04-22 08:21:52 -0300

    added concur/react/

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./components/auth/Register";
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
 import React, { Fragment } from "react";
-import Navbar from "./components/Navbar";
-import Landing from "./components/landing";
+import Navbar from "./components/layout/Navbar";
 
 const App = () => {
 	return (
-		<Fragment>
-			<Navbar />
-			<Landing />
-		</Fragment>
+		<Router>
+			<Fragment>
+				<Navbar />
+				<section className="container">
+					<Routes>
+						<Route exact path="/" component={Landing} />
+						<Route exact path="/register" component={Register} />
+						<Route exact path="/login" component={Login} />
+					</Routes>
+				</section>
+			</Fragment>
+		</Router>
 	);
 };
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //server file that works
-const express = require('express');
-const connectDB = require('./config/db');
-const path = require('path');
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -12,11 +12,10 @@ connectDB();
 app.use(express.json());
 
 // Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
-
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5001;
 
