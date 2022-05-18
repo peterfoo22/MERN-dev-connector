7d1d25e635a8f1526f4ed42d8926f011cfb89d4a
peterfoo22 <psuh22@gmail.com>
2022-05-18 10:43:15 +0200

    updated but action not working

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux';
+import { getCurrentProfiles } from '../../actions/profile';
+import { useEffect } from 'react';
 
-const Dashboard = props => {
+const Dashboard = ({getCurrentProfile, auth, profile}) => {
+  useEffect(()=>{
+    getCurrentProfiles();
+  },[]);
+
   return (
     <div>Dashboard</div>
   )
 }
 
-Dashboard.propTypes = {}
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+}
 
-export default Dashboard
+const mapStateToProps = state => ({
+  auth: state.auth,
+  profile: state.profile
+})
+
+export default connect(mapStateToProps, {getCurrentProfiles}) (Dashboard);
