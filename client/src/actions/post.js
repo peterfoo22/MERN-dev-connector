015f8955bff2307943a62fcf36469dc7c13c6e20
peterfoo22 <psuh22@gmail.com>
2022-06-07 11:25:03 +0200

    updated posts with like/unlike

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,27 +1,86 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import {
-  GET_POSTS,
-  POST_ERROR
-} from './types'
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
 
 //get posts
 
-export const getPosts = () => async dispatch => {
-  try {
-    const res = await axios.get("http://localhost:5001/api/posts")
+export const getPosts = () => async (dispatch) => {
+	try {
+		const res = await axios.get("http://localhost:5001/api/posts");
 
-    console.log('Success- ALL POSTS', res.data)
+		console.log("Success- ALL POSTS", res.data);
 
-    dispatch({
-      type: GET_POSTS,
-      payload: res.data
-    })
+		dispatch({
+			type: GET_POSTS,
+			payload: res.data,
+		});
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status },
+		});
+	}
+};
 
-  } catch (err) {
-    dispatch({
-      type: POST_ERROR,
-      payload: {msg: err.response.statusText, status: err.response.status}
-    })
-  }
-}
+//ADD LIKES
+
+export const addLike = (postID) => async (dispatch) => {
+	try {
+		// could not use the axios.post command as it as not working, used the fetch command instead
+		await fetch(`http://localhost:5001/api/posts/like/${postID}`, {
+			method: "PUT", // or 'PUT'
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json",
+				"x-auth-token": `${localStorage.token}`,
+			},
+		})
+			.then((response) => {
+				if (response.ok) {
+					response.json();
+				}
+        dispatch(setAlert("Already Liked", "Fail"));
+
+        return Promise.reject(response);
+
+			})
+			.then((data) => {
+				dispatch({
+					type: UPDATE_LIKES,
+					payload: { postID, likes: data },
+				});
+
+				dispatch(setAlert("Like Added", "Success"));
+			})
+			.catch((error) => {});
+	} catch (err) {
+		console.error("Error:", err);
+		dispatch({
+			type: POST_ERROR,
+			payload: {
+				msg: err.response.status.text,
+				status: err.response.status,
+			},
+		});
+	}
+};
+
+export const removeLike = (postID) => async (dispatch) => {
+	try {
+		const res = await axios.put(
+			`http://localhost:5001/api/posts/unlike/${postID}`
+		);
+
+		console.log("Success- Added Unlike Likee", res.data);
+
+		dispatch({
+			type: UPDATE_LIKES,
+			payload: { postID, likes: res.data },
+		});
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status },
+		});
+	}
+};
