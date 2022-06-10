7deeed1f1b114852ff28f1e34299073263071b2c
peterfoo22 <psuh22@gmail.com>
2022-06-10 10:25:53 +0200

    updated for deploy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/db");
 const path = require("path");
 var cors = require('cors')
 const app = express();
+const path = require('path');
 
 app.use(cors())
 
@@ -19,6 +20,17 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+  //set static folder 
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html' ))
+  })
+
+}
 
 const PORT = process.env.PORT || 5001;
 
